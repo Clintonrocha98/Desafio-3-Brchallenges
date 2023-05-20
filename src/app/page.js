56fb6a6b04d6af79cb70c522afa2d71184bd0323
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2023-05-20 05:25:56 -0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,18 @@ import { Banner } from "@/components/Banner";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { MiniCard } from "@/components/MiniCard";
-import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <link rel="icon" href="public/assets/favicon.ico" sizes="any" />
+            </Head>
             <Header />
             <Banner />
             <MiniCard />
             <Footer />
-            <Analytics />
         </>
     );
 }
